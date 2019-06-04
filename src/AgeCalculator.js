9835b552eb7714824cdfabbcfbf6f20ee3dab6f3
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-06-03 22:14:32 -0700

    Add jupiter, mars and venus fucntions

--- src/AgeCalculator.js
+++ src/AgeCalculator.js
@@ -26,5 +26,28 @@ AgeCalculator.prototype.checkMercuryYears = function(){
   let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
   let newYearMercury = Math.floor(ageEarth / 31536000);
   return Math.floor(newYearMercury / .24);
+};
 
+AgeCalculator.prototype.checkVenusYears = function(){
+  let currentDate = new Date();
+  let userInput = new Date(this.date);
+  let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
+  let newYearMercury = Math.floor(ageEarth / 31536000);
+  return Math.floor(newYearMercury / .62);
+};
+
+AgeCalculator.prototype.checkMarsYears = function(){
+  let currentDate = new Date();
+  let userInput = new Date(this.date);
+  let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
+  let newYearMercury = Math.floor(ageEarth / 31536000);
+  return Math.floor(newYearMercury / 1.88);
+};
+
+AgeCalculator.prototype.checkJupiterYears = function(){
+  let currentDate = new Date();
+  let userInput = new Date(this.date);
+  let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
+  let newYearMercury = Math.floor(ageEarth / 31536000);
+  return Math.floor(newYearMercury / 11.86);
 };
