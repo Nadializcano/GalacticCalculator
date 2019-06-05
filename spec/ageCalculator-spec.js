1ebcedb0654fdd1b90996770a82fc93692f44886
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-06-04 21:27:30 -0700

    Add 6th test and logic, Add logic for surpassed life on other planets

--- spec/ageCalculator-spec.js
+++ spec/ageCalculator-spec.js
@@ -7,32 +7,38 @@ describe('AgeCalculator', function() {
   });
 
 
-  it('should test the difference between age and date in seconds', function() {
+  it('it should test the difference between age and date in seconds', function() {
     let userInput = new AgeCalculator('10-31-1988');
     let age = userInput.checkDate();
     expect(age - userInput).toEqual(userInput - age);
   });
-  it('should test the age in seconds', function(){
+  it('it should test the age in seconds', function(){
     let userInput = new AgeCalculator('10-31-1988');
     let date = new Date(userInput.date);
     let currentDate = new Date();
     expect(userInput.checkDate()).toEqual((currentDate.getTime() - date.getTime()) / 1000);
   });
 
-  it('should test that the program gets years', function(){
+  it('it should test that the program gets years', function(){
     let userInput = new AgeCalculator('1988-10-31');
     expect(userInput.checkEarthYear()).toEqual(30);
   });
 
-  it('should test that the program returns years on mercury years', function(){
+  it('it should test that the program returns years on mercury years', function(){
     let userInput = new AgeCalculator('10-31-1988');
 
     expect(userInput.checkMercuryYears()).toEqual(125);
    });
 
-   it('should test the life expectancy in mercury', function(){
+   it('it should test the life expectancy in mercury', function(){
      let userAge = new AgeCalculator('1988-10-31', 30, 96);
 
      expect(userAge.lifeExpectancyMercury()).toEqual(400);
     });
+
+    it('it should test surpassed age in mercury', function(){
+      let userAge = new AgeCalculator('1922-10-31', 97, 96);
+
+      expect(userAge.lifeExpectancyMercury()).toEqual('Your surpassed the life expectancy by' + 4);
+     });
 });
--- src/AgeCalculator.js
+++ src/AgeCalculator.js
@@ -33,46 +33,65 @@ AgeCalculator.prototype.checkVenusYears = function(){
   let currentDate = new Date();
   let userInput = new Date(this.date);
   let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
-  let newYearMercury = Math.floor(ageEarth / 31536000);
-  return Math.floor(newYearMercury / .62);
+  let newYearVenus = Math.floor(ageEarth / 31536000);
+  return Math.floor(newYearVenus / .62);
 };
 
 AgeCalculator.prototype.checkMarsYears = function(){
   let currentDate = new Date();
   let userInput = new Date(this.date);
   let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
-  let newYearMercury = Math.floor(ageEarth / 31536000);
-  return Math.floor(newYearMercury / 1.88);
+  let newYearMars = Math.floor(ageEarth / 31536000);
+  return Math.floor(newYearMars / 1.88);
 };
 
 AgeCalculator.prototype.checkJupiterYears = function(){
   let currentDate = new Date();
   let userInput = new Date(this.date);
   let ageEarth = Math.floor((currentDate.getTime() - userInput.getTime()) / 1000);
-  let newYearMercury = Math.floor(ageEarth / 31536000);
-  return Math.floor(newYearMercury / 11.86);
+  let newYearJupiter = Math.floor(ageEarth / 31536000);
+  return Math.floor(newYearJupiter / 11.86);
 };
 
 AgeCalculator.prototype.lifeExpectancyMercury = function(){
   let lifeExpected = Math.floor(this.life / .24);
+  let userInput = Math.floor(this.age / .24);
+  let surpassed = userInput - lifeExpected;
+  if (userInput > lifeExpected) {
+    return 'Your surpassed the life expectancy by' + surpassed;
+  } else {
 
   return lifeExpected;
+}
 };
 
 AgeCalculator.prototype.lifeExpectancyVenus = function(){
   let lifeExpected = Math.floor(this.life / .62);
+  let userInput = Math.floor(this.age / .62);
+  let surpassed = userInput - lifeExpected;
+  if (userInput > lifeExpected) {
+    return 'Your surpassed the life expectancy by' + surpassed;
+  } else {
 
   return lifeExpected;
 };
 
 AgeCalculator.prototype.lifeExpectancyMars = function(){
   let lifeExpected = Math.floor(this.life / 1.88);
-
+  let userInput = Math.floor(this.age / 1.88);
+  let surpassed = userInput - lifeExpected;
+  if (userInput > lifeExpected) {
+    return 'Your surpassed the life expectancy by' + surpassed;
+  } else {
   return lifeExpected;
 };
 
 AgeCalculator.prototype.lifeExpectancyJupiter = function(){
   let lifeExpected = Math.floor(this.life / 11.86);
-
+  let userInput = Math.floor(this.age / 11.86);
+  let surpassed = userInput - lifeExpected;
+  if (userInput > lifeExpected) {
+    return 'Your surpassed the life expectancy by' + surpassed;
+  } else {
   return lifeExpected;
 };
